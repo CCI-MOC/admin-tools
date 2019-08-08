e507770ee0d47a3f54f130eba032e9db7ba009f7
Naved Ansari <naved001@gmail.com>
2019-08-08 14:24:14 -0400

    Refactor code and add support for nexus 6000
    * Rename backup_config to run_command. So we now pass the command to be run
    on the switch as an argument.
    * Use % formatting in logging functions and pass the % parameters as arguments
    * Fix import order.

--- switch-backups/switch_backup.py
+++ switch-backups/switch_backup.py
@@ -1,39 +1,41 @@
 """This script will logon to a switch and copy the running configuraion file to a tftp server"""
 
 import json
-import pexpect
 import logging
 import logging.handlers
 from datetime import datetime
+import pexpect
 
 
-def setup_logging(name, log_file):
+def setup_logging(name, logfile):
     """Setup logger with some custom formatting"""
     formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    new_logger = logging.getLogger(name)
+    new_logger.setLevel(logging.DEBUG)
     handler = logging.handlers.RotatingFileHandler(
-        log_file, mode="a", maxBytes=5 * 2**20)
+        logfile, mode="a", maxBytes=5 * 2**20)
     handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+    new_logger.addHandler(handler)
+    return new_logger
 
 
-def backup_config(hostname, username, password, switch_type):
-    """Run the copy config command on a switch"""
+def run_command(hostname, username, password, command):
+    """Run command on a switch"""
+
+    # 1. Begin login process
     console = pexpect.spawn("ssh {}@{}".format(username, hostname))
     alternatives = ["Permission denied",
                     "Connection reset by peer\r\r\n", "[Pp]assword:*"]
     outcome = console.expect(alternatives)
     if outcome == 0:
-        logger.error("Permission denied for " + hostname)
-        console.kill()
+        logger.error("Permission denied for %s", hostname)
+        console.close()
         return
     elif outcome == 1:
-        logger.error("Connection reset by: {}".format(hostname))
-        logger.error("console.before: " + console.before)
-        logger.error("console.after:" + console.after)
+        logger.error("Connection reset by: %s", hostname)
+        logger.error("console.before: %s", console.before)
+        logger.error("console.after: %s", console.after)
         console.close()
         return
     elif outcome == 2:
@@ -52,12 +54,11 @@ def backup_config(hostname, username, password, switch_type):
         console.sendline("enable")
         console.expect("#")
 
-    command = commands[switch_type] + \
-        "_{}_{}".format(hostname, datetime.now().strftime("%a"))
-
+    # 2. Successful login. Attempt to run the command now
     console.sendline(command)
     console.expect("#", timeout=400)
 
+    # 3. Logout process begins.
     console.sendline('exit')
     alternatives = [pexpect.EOF, '>']
     if console.expect(alternatives):
@@ -66,33 +67,42 @@ def backup_config(hostname, username, password, switch_type):
 
 
 def main():
-    """Main main"""
-    for switch in config["switches"]:
+    """Main man"""
+
+    tftp_server = CONFIG["tftp_server"]
+
+    commands = {
+        "vdx6740": "copy running-config tftp://@{}/{}",
+        "nexus6000": "copy running-config tftp://{}/{} vrf management",
+        "icx6610": "copy running-config tftp {} {}",
+        "ibm_rack": "copy running-config tftp address {} filename {}",
+    }
+
+    for switch in CONFIG["switches"]:
+        hostname = switch["hostname"]
+        username = switch["username"]
+        password = switch["password"]
+        switch_type = switch["switch_type"]
+
+        filename = "config" + \
+            "_{}_{}".format(hostname, datetime.now().strftime("%a"))
+
+        command = commands[switch_type].format(tftp_server, filename)
+        print(command)
         try:
-            backup_config(switch["hostname"], switch["username"],
-                          switch["password"], switch["switch_type"])
-        except pexpect.exceptions.TIMEOUT as e:
-            logger.error("\nHost {} timedout".format(switch["hostname"]))
-            logger.exception(e)
-        except pexpect.exceptions.EOF as e:
-            logger.error("\nFailed to connect to {}".format(
-                switch["hostname"]))
-            logger.exception(e)
+            run_command(hostname, username, password, command)
+        except pexpect.exceptions.TIMEOUT as error:
+            logger.error("\nHost %s timed out", hostname)
+            logger.exception(error)
+        except pexpect.exceptions.EOF as error:
+            logger.error("\nFailed to connect to %s", hostname)
+            logger.exception(error)
 
 
 if __name__ == "__main__":
     with open('config.json') as config_file:
-        config = json.load(config_file)
+        CONFIG = json.load(config_file)
 
-    tftp_server = config["tftp_server"]
-
-    commands = {
-        "vdx6740": "copy running-config tftp://@{}/config".format(tftp_server),
-        "nexus6000": "copy running-config tftp://{}/config".format(tftp_server),
-        "icx6610": "copy running-config tftp {} config".format(tftp_server),
-        "ibm_rack": "copy running-config tftp address {} filename config".format(tftp_server)
-    }
-
-    log_file = "/var/log/switch_backups.log"
-    logger = setup_logging(__name__, log_file)
+    LOG_FILE = "/var/log/switch_backups.log"
+    logger = setup_logging(__name__, LOG_FILE)
     main()
