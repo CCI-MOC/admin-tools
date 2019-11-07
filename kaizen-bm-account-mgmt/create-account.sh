c75ba27f74cca494e3a5f12e69ab0a840a7c0604
Naved Ansari <naved001@gmail.com>
2019-11-07 15:15:48 -0500

    Use tee to write to file because sudo echo doesn't make sense
    run the bmi project create command using sudo -E so that
    we can actually write to the database (because bmi is stupid).

--- kaizen-bm-account-mgmt/create-account.sh
+++ kaizen-bm-account-mgmt/create-account.sh
@@ -1,35 +1,30 @@
 #! /bin/bash
 
-set -e
+set -ex
 
 if [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$#" -ne 2 ]; then
     echo "Run this as ./create-account.sh PROJECT USER"
     exit 1
 fi
 
-su -l $USERNAME
-exit
-
-if [ ! -f "/home/$USERNAME/.bashrc" ]; then
-    echo "The home directory or bashrc does not exist"
-    exit 1
-fi
+USERNAME=$2
+echo $USERNAME
 
 PROJECT=$1
-USERNAME=$2
 PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
+echo $PASSWORD
 NETWORK="bmi-pro"
 
 hil project list-networks $PROJECT || (echo "Creating project" && hil project create $PROJECT)
 hil user create $USERNAME $PASSWORD regular
 hil user project add $USERNAME $PROJECT
-hil network grant-access $NETWORK $PROJECT
-bmi project create $PROJECT
+hil network grant-access $NETWORK $PROJECT || true
+sudo -E bmi project create $PROJECT
 
 
-echo "export HIL_USERNAME=$USERNAME" >> /home/$USERNAME/.bashrc
-echo "export HIL_PASSWORD=$PASSWORD" >> /home/$USERNAME/.bashrc
-echo "echo 'Your HIL project is $PROJECT. You can remove this message from your bashrc'" >> /home/$USERNAME/.bash_profile
+echo "export HIL_USERNAME=$USERNAME" |sudo tee -a /home/$USERNAME/.bashrc
+echo "export HIL_PASSWORD=$PASSWORD" |sudo tee -a  /home/$USERNAME/.bashrc
+echo "echo 'Your HIL project is $PROJECT. You can remove this message from your bashrc'" |sudo tee -a /home/$USERNAME/.bashrc
 echo "Done"
 
 exit 0
