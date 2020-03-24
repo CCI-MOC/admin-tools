8d195e5335cd65489525fadbcf49f96dc1f9915c
Radoslav Milanov <radoslav.milanov@gmail.com>
2020-03-24 18:05:27 -0400

    Adding support for CentOS 8/octopus

--- Ceph/InstallClient.sh
+++ Ceph/InstallClient.sh
@@ -1,11 +1,12 @@
 release=nautilus
+osver=grep -i version_id /etc/os-release | cut -d \" -f 2
 tput bold
 if [ -z ${1+x} ]; then echo Will use default release - $release; else release=$1;echo Will use custom release - $release;fi
 tput sgr0
 grep timeout /etc/yum.conf &> /dev/null || sudo sh -c 'echo "timeout=5" >> /etc/yum.conf'
 grep options /etc/resolv.conf &> /dev/null || sudo sh -c 'echo "options single-request" >> /etc/resolv.conf'
 sudo yum -y install epel-release deltarpm
-sudo yum -y install http://download.ceph.com/rpm-$release/el7/noarch/ceph-release-1-1.el7.noarch.rpm
+sudo yum -y install http://download.ceph.com/rpm-$release/el$osver/noarch/ceph-release-1-1.el7.noarch.rpm
 sudo yum -y update
 sudo yum -y install ceph-common
 clear
