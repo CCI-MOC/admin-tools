ecb95c2049878249f9881b8b06b42b2e108e6f04
Radoslav Milanov <radoslav.milanov@gmail.com>
2020-03-26 12:54:45 -0400

    Update InstallClient.sh

--- Ceph/InstallClient.sh
+++ Ceph/InstallClient.sh
@@ -6,7 +6,7 @@ tput sgr0
 grep timeout /etc/yum.conf &> /dev/null || sudo sh -c 'echo "timeout=5" >> /etc/yum.conf'
 grep options /etc/resolv.conf &> /dev/null || sudo sh -c 'echo "options single-request" >> /etc/resolv.conf'
 sudo yum -y install epel-release deltarpm
-sudo yum -y install http://download.ceph.com/rpm-$release/el$osver/noarch/ceph-release-1-1.el7.noarch.rpm
+sudo yum -y install http://download.ceph.com/rpm-$release/el$osver/noarch/ceph-release-1-1.el$osver.noarch.rpm
 sudo yum -y update
 sudo yum -y install ceph-common
 clear
